bc137d51b27f21a19691aa01fe25414553f7d09f
david-on-debian <secret321king3@gmail.com>
2024-10-21 13:10:01 -0500

    Add files via upload

--- splash.py
+++ splash.py
@@ -1,48 +1,43 @@
 #!/usr/bin/python3
 
-import warnings
-import os
-import sys
-import io
 
-i = input("splash 1.0 -#$ ")
-test = False
+__version__ = '1.1.c'
+version = '1.1'
+name = "splash"
 
-
-help = """
-----SPLASH 1.0.b help pages----
+help_part1 = f"""
+----{name.upper() + ' ' + __version__} help pages----
 when you start splash you get a prompt:
 
-splash 1.0 -#$ 
+{name + ' ' + version} -#$ 
 
 at this prompt you enter commands or operations.
 for example, say you want to add 2 + 2; you would then
 enter the following at the shell prompt:
 
-splash 1.0 -#$ 2 + 2
+{name + ' ' + version} -#$ 2 + 2
 
 and when you press enter; you get this output:
 
-splash 1.0 operation: add; output -# 4
+{name + ' ' + version} operation: add; output -# 4
 
 and don`t worry! there are more math operands you can use.
 such as:
-
-splash 1.0 -#$ 2 + 2
-splash 1.0 -#$ 2 - 2
-splash 1.0 -#$ 2 * 2
-splash 1.0 -#$ 2 / 2
+"""
+help_part2 = f""" 
+{name + ' ' + version} -#$ 2 + 2
+{name + ' ' + version} -#$ 2 - 2
+{name + ' ' + version} -#$ 2 * 2
+{name + ' ' + version} -#$ 2 / 2
 and, last but not least,
-splash 1.0 -#$ 2^2 
+{name + ' ' + version} -#$ 2 ^ 2 
 
 you can also use the `now` command in math operations!
 the syntax and semantics  of this command are quite simple.
-(please note ``now [number]^[number]`` is not allowed, nor is
-''2^2 now [number] [operator] [number]``)
 it works as follows:
 
-splash 1.0 -#$ 2 + 2 now * 2
-splash 1.0 operation: add; output -# 8
+{name + ' ' + version} -#$ 2 + 2 now * 2
+{name + ' ' + version} operation: add; output -# 8
 
 you get this output because 2 plus 2 equals 4, which multiplied by 2 equals 8.
 
@@ -50,24 +45,24 @@ And no terminal/shell would be complete without variables!
 the basic syntax for defining a variable is as follows:
 
 var [VARIABLE-NAME] = [VARIABLE-VALUE]
-
+"""
+help_part3 = f"""
 for example;
 
-splash 1.0 -#$ var name = 'david goeke'
-splash 1.0 -#$ print name
+{name + ' ' + version} -#$ var name = 'david goeke'
+{name + ' ' + version} -#$ print name
 'david goeke'
 
-splash 1.0 -#$ var number = 248
-splash 1.0 -#$ print number
+{name + ' ' + version} -#$ var number = 248
+{name + ' ' + version} -#$ print number
 248
 
 etc, etc.
-
 """
 
 license = """
-As of yet, there is no offical license for this project,
-I may inculde it later.
+As of yet, there is no official license for this project,
+I may include it later.
 """
 
 class Data:
@@ -84,14 +79,23 @@ class Data:
         variable_name = variable
         self.variables[variable_name] = value
 
+#def to_list(value):
+#    value = list(value)
+#    if value[0] == '[':
+#        value.pop(0)
+#    if value[value.__len__] == ']':
+#        value.pop(value.__len__)
+#    for char in value:
+        
+        
 
 
 variables = Data()
 
-
+test = False
 
 while True:
-    
+    i = input(f"{name + ' ' + version} -#$ ")
     # development option, i recommend you don`t use this. 
     if test:
         holder = []
@@ -104,7 +108,6 @@ while True:
         for g in enumerate(i):
             holder.append(g)
         print(holder)
-        i = input("splash 1.0 -#$ ")
         continue
         
     elif "quit" in i:
@@ -114,16 +117,74 @@ while True:
     elif "exit" in i:
         print("Goodbye!")
         break
-        
+    
     elif len(i) == 1:
         if str(i[0]) in variables.read_all():
             print(variables.read(i[0]))
-            i = input("splash 1.0 -#$ ")
             continue
         print(i[0])
-        i = input("splash 1.0 -#$ ")
         continue
                  
+    elif " == " in i:
+        equals = i.index(" == ")
+        first = i[:equals]
+        last = i[equals + 4:]
+        if first in variables.read_all():
+            first = variables.read(first)
+        if last in variables.read_all():
+            last = variables.read(last)
+        if first == last:
+            print(f"{name + ' ' + version} boolean; output: -# True")
+            continue
+        else:
+            print(f"{name + ' ' + version} boolean; output: -# False")
+            continue
+
+    elif " != " in i:
+        equals = i.index(" != ")
+        first = i[:equals]
+        last = i[equals + 4:]
+        if first in variables.read_all():
+            first = variables.read(first)
+        if last in variables.read_all():
+            last = variables.read(last)
+        if first != last:
+            print(f"{name + ' ' + version} boolean; output: -# True")
+            continue
+        else:
+            print(f"{name + ' ' + version} boolean; output: -# False")
+            continue
+
+    elif " < " in i:
+        equals = i.index(" < ")
+        first = i[:equals]
+        last = i[equals + 4:]
+        if first in variables.read_all():
+            first = variables.read(first)
+        if last in variables.read_all():
+            last = variables.read(last)
+        if first > last:
+            print(f"{name + ' ' + version} boolean; output: -# True")
+            continue
+        else:
+            print(f"{name + ' ' + version} boolean; output: -# False")
+            continue
+
+    elif " > " in i:
+        equals = i.index(" > ")
+        first = i[:equals]
+        last = i[equals + 4:]
+        if first in variables.read_all():
+            first = variables.read(first)
+        if last in variables.read_all():
+            last = variables.read(last)
+        if first < last:
+            print(f"{name + ' ' + version} boolean; output: -# True")
+            continue
+        else:
+            print(f"{name + ' ' + version} boolean; output: -# False")
+            continue
+
     elif i[0:4] == "var ":
         assign = i.find(' = ') 
         value = i[assign + 3:]
@@ -135,10 +196,10 @@ while True:
             elif value.startswith("\"") and value.endswith("\""):
                 pass
             elif value.startswith("[") and value.endswith("]"):
+ #               value = to_list(value)
                 value = list(value)
             elif value.startswith("{") and value.endswith("}"):
-                print("dictionary's are not yet supported by splash 1.0.b.")
-                i = input("splash 1.0 -#$ ")
+                print(f"dictionary's are not yet supported by {name + ' ' + version}.")
                 continue
                 
             else:
@@ -150,13 +211,12 @@ while True:
                     except:
                         print(f"error: {value} not a variable or number, and no matching quotation marks found")
                         del value
-                        i = input("splash 1.0 -#$ ")
                         continue
-        name = i[4:assign]
-        variables.write(name, value)
-        i = input("splash 1.0 -#$ ")
+        var_name = i[4:assign]
+        variables.write(var_name, value)
         continue
        
+    # A pseudo-developement option, feel free to tinker around with it.
     elif "type" in i:
         t = i.index("type")
         if str(i[t + 5:]) in variables.read_all():
@@ -164,12 +224,10 @@ while True:
         else:
             value = i[t + 5:]
         print(type(value))
-        i = input("splash 1.0 -#$ ")
         continue
        
     elif "license" in i:
         print(license)
-        i = input("splash 1.0 -#$ ")
         continue
 
     elif " + " in i[0:10] != None:
@@ -202,8 +260,7 @@ while True:
             except:
                 num1 = float(num1)
         except:
-            print(f"{num0}, and/or {num1}, (or the values of the former,) are not int(s) or float(s). strign concatenation is not yet supported.")
-            i = input("splash 1.0 -#$ ")
+            print(f"{num0}, and/or {num1}, (or the values of the former,) are not int(s) or float(s). string concatenation is not yet supported.")
             continue
             
         if n != None:            
@@ -233,13 +290,17 @@ while True:
                 except:
                     num = float(i[n + 7:n + 100000])
                 rv = rv / num
+            elif operand == " ^ ":
+                try:
+                    num = int(i[n + 7:])
+                except:
+                    num = float(i[n + 7:])
+                rv = rv ** num
             else:
                 rv = num0 + num1
         else:
             rv = num0 + num1
-        print(f"splash 1.0 operation: add; output: -# {rv}")
-        
-        i = input("splash 1.0 -#$ ")
+        print(f"{name + ' ' + version} operation: add; output: -# {rv}")
         continue
         
     elif " - " in i[0:10] != None:
@@ -272,8 +333,7 @@ while True:
             except:
                 num1 = float(num1)
         except:
-            print(f"{num0}, and/or {num1}, (or the values of the former,) are not int(s) or float(s). \n strign concatenation is not yet supported.")
-            i = input("splash 1.0 -#$ ")
+            print(f"{num0}, and/or {num1}, (or the values of the former,) are not int(s) or float(s). \n string concatenation is not yet supported.")
             continue
             
         if n != None:            
@@ -303,13 +363,17 @@ while True:
                 except:
                     num = float(i[n + 7:n + 100000])
                 rv = rv / num
+            elif operand == " ^ ":
+                try:
+                    num = int(i[n + 7:])
+                except:
+                    num = float(i[n + 7:])
+                rv = rv ** num
             else:
                 rv = num0 - num1
         else:
             rv = num0 - num1
-        print(f"splash 1.0 operation: subtract; output: -# {rv}")
-        
-        i = input("splash 1.0 -#$ ")
+        print(f"{name + ' ' + version} operation: subtract; output: -# {rv}")
         continue
     
     elif " * " in i[0:10] != None:
@@ -342,8 +406,7 @@ while True:
             except:
                 num1 = float(num1)
         except:
-            print(f"{num0}, and/or {num1}, (or the values of the former,) are not int(s) or float(s). strign concatenation is not yet supported.")
-            i = input("splash 1.0 -#$ ")
+            print(f"{num0}, and/or {num1}, (or the values of the former,) are not int(s) or float(s). string concatenation is not yet supported.")
             continue
             
         if n != None:            
@@ -373,13 +436,17 @@ while True:
                 except:
                     num = float(i[n + 7:n + 100000])
                 rv = rv / num
+            elif operand == " ^ ":
+                try:
+                    num = int(i[n + 7:])
+                except:
+                    num = float(i[n + 7:])
+                rv = rv ** num
             else:
                 rv = num0 * num1
         else:
             rv = num0 * num1
-        print(f"splash 1.0 operation: multiply; output: -# {rv}")
-        
-        i = input("splash 1.0 -#$ ")
+        print(f"{name + ' ' + version} operation: multiply; output: -# {rv}")
         continue
         
     elif " / " in i[0:10] != None:
@@ -412,8 +479,7 @@ while True:
             except:
                 num1 = float(num1)
         except:
-            print(f"{num0}, and/or {num1}, (or the values of the former,) are not int(s) or float(s). strign concatenation is not yet supported.")
-            i = input("splash 1.0 -#$ ")
+            print(f"{num0}, and/or {num1}, (or the values of the former,) are not int(s) or float(s). string concatenation is not yet supported.")
             continue
             
         if n != None:            
@@ -443,19 +509,37 @@ while True:
                 except:
                     num = float(i[n + 7:n + 100000])
                 rv = rv / num
+            elif operand == " ^ ":
+                try:
+                    num = int(i[n + 7:])
+                except:
+                    num = float(i[n + 7:])
+                rv = rv ** num
             else:
                 rv = num0 / num1
         else:
             rv = num0 / num1
-        print(f"splash 1.0 operation: divide; output: -# {rv}")
-        
-        i = input("splash 1.0 -#$ ")
+        print(f"{name + ' ' + version} operation: divide; output: -# {rv}")
         continue
 
-    elif "^" in i:
-        pow_s = i.index("^")
-        num0 = i[:pow_s]
-        num1 = i[pow_s+1:]
+    elif " ^ " in i:
+        l = i.index(" ^ ")
+        n = None
+        num0 = i[:l]
+        num1 = i[l + 3:]
+        
+        try:
+            n = i.index(" now ")
+        except:
+            pass
+            
+        if n != None:
+            num1 = i[l + 3:n]
+        
+        if str(num0) in variables.read_all():
+            num0 = variables.read(num0)
+        elif str(num1) in variables.read_all():
+            num1 = variables.read(num1)
 
         try:
             try:
@@ -468,51 +552,83 @@ while True:
             except:
                 num1 = float(num1)
         except:
-            print(f"{num0}, and/or {num1}, (or the values of the former,) are not int(s) or float(s). \n string concatenation is not yet supported.")
-            i = input("splash 1.0 -#$ ")
+            print(f"{num0}, and/or {num1}, (or the values of the former,) are not int(s) or float(s). string concatenation is not yet supported.")
             continue
-
-        rv = pow(num0, num1)
-        print(f"splash 1.0 operation: to-the-power-of; output: -# {rv}")
-        i = input("splash 1.0 -#$ ")
+            
+        if n != None:            
+            rv = num0 ** num1
+            operand = i[n + 4:n + 7]
+            if operand == " + ":
+                try:
+                    num = int(i[n + 7:n + 100000])
+                except:
+                    num = float(i[n + 7:n + 100000])
+                rv = rv + num
+            elif operand == " - ":
+                try:
+                    num = int(i[n + 7:n + 100000])
+                except:
+                    num = float(i[n + 7:n + 100000])
+                rv = rv - num
+            elif operand == " * ":
+                try:
+                    num = int(i[n + 7:n + 100000])
+                except:
+                    num = float(i[n + 7:n + 100000])
+                rv = rv * num
+            elif operand == " / ":
+                try:
+                    num = int(i[n + 7:n + 100000])
+                except:
+                    num = float(i[n + 7:n + 100000])
+                rv = rv / num
+            elif operand == " ^ ":
+                try:
+                    num = int(i[n + 7:])
+                except:
+                    num = float(i[n + 7:])
+                rv = rv ** num
+            else:
+                rv = num0 ** num1
+        else:
+            rv = num0 ** num1
+        print(f"{name + ' ' + version} operation: exponent; output: -# {rv}")
         continue
         
     elif "help" in i:
-        print(help)
-        i = input("splash 1.0 -#$ ")
+        print(help_part1)
+        c = input("Press Enter/Return To Continue: ")
+        print(help_part2)
+        c = input("Press Enter/Return To Continue: ")
+        print(help_part3)
         continue
         
     elif "man" in i:
-        print(help)
-        i = input("splash 1.0 -#$ ")
+        print(help_part1)
+        c = input("Press Enter/Return To Continue: ")
+        print(help_part2)
+        c = input("Press Enter/Return To Continue: ")
+        print(help_part3)
         continue
         
     elif "print" in i:
         print_ = i.find("print")
-        
-            
+              
         if str(i[print_ + 6:]) in variables.read_all():
             print(variables.read(str(i[print_ + 6:])))
-            i = input("splash 1.0 -#$ ")
             continue
         else:
             pv = str(i[print_ + 6:])
             print(f"variable {pv} does not exist")
-            i = input("splash 1.0 -#$ ")
             continue
             
     elif i[:] in variables.read_all():
         print(variables.read(str(i)))
-        i = input("splash 1.0 -#$ ")
         continue
             
     elif i == "":
-        i = input("splash 1.0 -#$ ")
         continue
         
     else:
         print("Unknown command, keyword, or operation.")
-        i = input("splash 1.0 -#$ ")
         continue
-        
-        
